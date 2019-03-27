0c1b9b420da81559c1604091aae4d727fd2f8c12
Nagornykh Maria <manyaknag@gmail.com>
2019-03-27 10:22:20 +0300

    настройки окружения

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 
+// import '~normalize.css/normalize';
+
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App)
-}).$mount('#app');
+}).$mount('[data-id="app"]');
