7e8eacd498e96254fc6c82f4c2db3e598db2c054
Nagornykh Maria <manyaknag@gmail.com>
2019-03-27 11:07:50 +0300

    разделила на компоненты

--- src/views/MarkedEntities.vue
+++ src/views/MarkedEntities.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="main__wrapper">
     <div class="main">
-      <div class="main__text"></div>
-      <div class="main__checkboxes"></div>
+      <div class="main__text"><Text /></div>
+      <div class="main__checkboxes"><Checkboxes /></div>
     </div>
-    <div class="result"></div>
+    <div class="result"><Result /></div>
   </div>
 </template>
 
 <script>
+  import Text from '@/components/Text.vue'
+  import Checkboxes from '@/components/Checkboxes.vue'
+  import Result from '@/components/Result.vue'
+
   export default {
     name: 'MarkedEntities',
-    props: {}
+    props: {},
+    components: {
+      Text,
+      Checkboxes,
+      Result
+    }
   };
 </script>
 
