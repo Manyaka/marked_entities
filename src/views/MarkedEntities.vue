a2d1ac0ca919bebb83145a36caa38afc2578a819
Nagornykh Maria <manyaknag@gmail.com>
2019-03-27 15:15:43 +0300

    json

--- src/views/MarkedEntities.vue
+++ src/views/MarkedEntities.vue
@@ -2,21 +2,66 @@
   <div class="main__wrapper">
     <div class="main">
       <div class="main__text">{{ mainText }}</div>
-      <div class="main__checkboxes">{{ mainCheckboxes }}</div>
+      <div class="main__checkboxes">
+        <ul class="checkboxes-list">
+          <li class="checkboxes-list__item" v-for="checkbox in mainCheckboxes"
+              v-bind:key="checkbox.id">
+            <span class="pseudo-checkbox"></span> {{ checkbox.label }}
+          </li>
+        </ul>
+      </div>
     </div>
     <div class="result">{{ mainResult }}</div>
   </div>
 </template>
 
 <script>
+  // import json from '/json/mainText.json'
+  // import json2 from '/json/mainCheckboxes.json'
+
+  //получить данные из json
+  // let mainText = require('@/json/mainText.json');
+  /*let mainCheckboxes = fetch('https://api.github.com/users/chriscoyier/repos')
+    .then(response => response.json())
+    .then(data => {
+    console.log('data=', data);
+    }
+  );
+  console.log('mainCheckboxes=', mainCheckboxes);*/
+  let mainText = "mainText";
+  let mainCheckboxes = [
+    {
+      "id": 0,
+      "label": "text"
+    },
+    {
+      "id": 1,
+      "label": "masha"
+    },
+    {
+      "id": 2,
+      "label": "vue"
+    }];
+
   export default {
     name: 'MarkedEntities',
     data() {
       return {
         mainText: 'mainText',
-        mainCheckboxes: 'mainCheckboxes',
-        mainResult: 'mainResult'
-      }
+        mainCheckboxes: mainCheckboxes,
+        mainResult: ''
+        // publicPath: process.env.BASE_URL
+      };
+    },
+    computed: {},
+    methods: {},
+    created() {
+      //получить данные из json
+      /*axios.get('https://www.cryptocompare.com/api/data/coinlist/')
+        .then(response => {
+          this.mainResult = response.data;
+          console.log(this.mainResult);
+      });*/
     }
   };
 </script>
