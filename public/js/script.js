65a05167c42b764a30e94e8641831250e261e8b3
Nagornykh Maria <manyaknag@gmail.com>
2019-04-01 11:35:04 +0300

    скрипт

--- public/js/script.js
+++ public/js/script.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
   let checkbox = document.querySelector('[data-selector="checkbox"]');
   let resultArray = [];
   let selection;
+  let arrLi = Array.from(checkboxList.children);
 
   document.onselectionchange = function() {
     selection = document.getSelection();
@@ -16,25 +17,52 @@ document.addEventListener('DOMContentLoaded', function(event) {
   });
 
   checkboxList.addEventListener('change', function() {
-    let arrLi = Array.from(checkboxList.children);
-
-    arrLi.forEach(function(element, i) {
+    arrLi.forEach(function(element) {
       let li = Array.from(element.children);
-
-      li.forEach(function(element, i) {
-        if (element.tagName === 'INPUT') {
-          mn(element);
-        }
-      });
+      mn(li, selection);
     });
   });
 });
 
-function mn(input) {
-  // console.log('mn=', input);
-  if (input.checked) {
-    console.log('Checkbox is checked');
-  } else {
-    console.log('Checkbox is not checked');
+//проверяем чекбокс на соответствие текста лейбла и выделенного, и чекаем
+function mn(elements, selection) {
+  let sel;
+  console.log('mn=', elements);
+
+  if (selection) {
+    sel = selection.toString();
+    console.log('sel=', sel);
   }
+
+  elements.forEach(function(element, i) {
+    let labelText;
+
+    if (element.tagName === 'LABEL') {
+      labelText = element.textContent;
+      console.log('labelText=', labelText);
+    }
+
+    if (labelText === sel) {
+      console.log('чекать можно');
+      if (element.tagName === 'INPUT') {
+        element.setAttribute('checked', 'checked');
+      }
+    } else {
+      console.log('чекать нельзя');
+      if (element.tagName === 'INPUT') {
+        element.removeAttribute('checked');
+      }
+    }
+  });
+
+  if (elements.tagName === 'INPUT') {
+    // mn(elements);
+  }
+
+  /*if (input.checked) {
+   console.log('Checkbox is checked');
+   } else {
+   console.log('Checkbox is not checked');
+   }*/
+  console.log('-------------');
 }
