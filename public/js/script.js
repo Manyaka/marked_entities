da6ac19f29f1df5553c094ef57eded8a01bbc812
Nagornykh Maria <manyaknag@gmail.com>
2019-04-03 16:31:25 +0300

    ie9

--- public/js/script.js
+++ public/js/script.js
@@ -8,6 +8,8 @@ document.addEventListener('DOMContentLoaded', function(event) {
   document.onselectionchange = function() {
     //селекшн должен считываться только в блоке текста - containsNode()
     if (document.getSelection().containsNode(textDiv, true)) {
+      textDiv.contentEditable = true;
+      textDiv.focus();
       selection = document.getSelection();
     }
   };
@@ -21,169 +23,54 @@ document.addEventListener('DOMContentLoaded', function(event) {
   //--------------------------------------------
 
   //--------------------------------------------
-  //selection, textDiv тоже используется в этом блоке
-  let checkboxList = document.querySelector('[data-selector="checkbox-list"]');
-  let arrLi = Array.from(checkboxList.children);
-
-  let range, clone;
-  let markNode = document.createElement('mark');
-  let spanNode = document.createElement('span');
-  let textDivInnerHtml = textDiv.innerHTML;
-
+  //selection тоже используется в этом блоке
   let buttons = document.querySelectorAll('[data-selector="buttons"]');
 
   buttons.forEach((btn) => {
-    btn.addEventListener('click', () => {
-      document.execCommand('hiliteColor', false, 'blue');
+    btn.addEventListener('click', (event) => {
+      console.log('event', event.target);
+      console.log('event', event.target.dataset);
+      console.log('event', event.target.dataset.color);
+      console.log('event', event.target.dataset.name);
+      document.execCommand('hiliteColor', false, event.target.dataset.color);
+      // highlight('blue');
     });
   });
 });
 
-function tn() {
-  if (!selection || selection.isCollapsed) {
-    window.alert('Сначала выделите текст');
-  } else {
-    // debugger;
-    // console.log('selectionStart=', selection);
-    // console.log('selection=', selection.toString());
-    range = selection.getRangeAt(0);
-    // console.log('rangeStart=', range);
-    // clone = range.cloneRange();
-    range.surroundContents(markNode);
-    // console.log('rangeAfterNode', range);
-    // selection.removeAllRanges();
-    // console.log('selectionMiddle=', selection);
-    // selection.addRange(clone);
-    // console.log('selectionEnd=', selection);
-  }
-}
-
-//проверяем чекбокс на соответствие текста лейбла и выделенного, и чекаем
-function mn(element, selection) {
-  let sel;
-  console.log('mn=', element, '**', typeof element); //object
-
-  if (selection) {
-    sel = selection.toString();
-    console.log('sel=', sel);
-  }
-  //element[0] - label
-  let labelText = element[0].textContent.trim();
-  console.log('labelText=', labelText);
-
-  let input = element[0].children[0];
-
-  if (labelText === sel) {
-    console.log('чекать можно');
-    input.setAttribute('checked', true);
-    console.log('input', input);
-  } else {
-    console.log('чекать нельзя');
-    input.removeAttribute('checked');
-    console.log('input', input);
-  }
-
-  /*element.forEach(function(element, i) {
-
-   if (element.tagName === 'LABEL') {
-   }
-
-   if (labelText === sel) {
-   console.log('чекать можно');
-   if (element.tagName === 'INPUT') {
-   element.setAttribute('checked', 'checked');
-   }
-   } else {
-   console.log('чекать нельзя');
-   if (element.tagName === 'INPUT') {
-   element.removeAttribute('checked');
-   }
-   }
-   });*/
-
-  /*if (elements.tagName === 'INPUT') {
-   // mn(elements);
-   }*/
-
-  /*if (input.checked) {
-   console.log('Checkbox is checked');
-   } else {
-   console.log('Checkbox is not checked');
-   }*/
-  console.log('-------------');
-}
-
-let selectionRange;
-
-function highlight(highlightID, color) {
-  if (window.getSelection && window.getSelection().toString()) {
-    let node = getSelectionParentElement(); //зачем получаем парента? Без него не работает
-    if (node != null) {
-      let text = getSelectionText();
-      console.log('Selected text: ' + text);
-      markFunc(node, text, /*HIGHLIGHT_CLASS + " " + */ color);
-    } else {
-      console.log('Parent node is null for some reason');
-    }
-  } else {
-    window.alert('Сначала выделите текст');
-    // console.log('tapped without selection');
-  }
-}
-
-function getSelectionText() {
-  if (window.getSelection) {
-    let sel = window.getSelection();
-    return sel.toString();
-  }
-}
-
-function getSelectionParentElement() {
-  let parentEl = null,
-    sel;
-  if (window.getSelection) {
+//для IE 9
+function makeEditableAndHighlight(colour) {
+  let range,
     sel = window.getSelection();
-    if (sel.rangeCount) {
-      selectionRange = sel.getRangeAt(0);
-      parentEl = selectionRange.commonAncestorContainer;
-      if (parentEl.nodeType !== 1) {
-        parentEl = parentEl.parentNode;
-      }
-    }
-  } else if ((sel = document.selection) && sel.type !== 'Control') {
-    parentEl = sel.createRange().parentElement();
+  if (sel.rangeCount && sel.getRangeAt) {
+    range = sel.getRangeAt(0);
   }
-  return parentEl;
+  document.designMode = 'on';
+  if (range) {
+    sel.removeAllRanges();
+    sel.addRange(range);
+  }
+  // Use HiliteColor since some browsers apply BackColor to the whole block
+  if (!document.execCommand('HiliteColor', false, colour)) {
+    document.execCommand('BackColor', false, colour);
+  }
+  document.designMode = 'off';
 }
 
-function markFunc(node, text, color) {
-  let instance = new Mark(node);
-
-  instance.mark(text, {
-    element: 'mark',
-    className: color,
-    separateWordSearch: false,
-    accuracy: 'partially',
-    diacritics: true,
-    acrossElements: true,
-    ignoreJoiners: true,
-    each: function(element) {
-      element.setAttribute('class', 'mn');
-    },
-    /*filter: function(node, term, totalCounter, counter) {
-      let res = false;
-      if (counter === 0) {
-        res = selectionRange.isPointInRange(node, selectionRange.startOffset);
-      } else {
-        res = selectionRange.isPointInRange(node, 1);
+function highlight(colour) {
+  let range, sel;
+  if (window.getSelection) {
+    // IE9 and non-IE
+    try {
+      if (!document.execCommand('BackColor', false, colour)) {
+        makeEditableAndHighlight(colour);
       }
-      console.log('Counter: ' + counter + ', startOffset: ' + selectionRange.startOffset);
-      return res;
-    },*/
-    done: function(totalMarks) {
-          window.getSelection().empty(); //This only in Chrome
-          console.log('total marks: ' + totalMarks);
-        },
-    debug: true
-  });
+    } catch (ex) {
+      makeEditableAndHighlight(colour);
+    }
+  } else if (document.selection && document.selection.createRange) {
+    // IE <= 8 case
+    range = document.selection.createRange();
+    range.execCommand('BackColor', false, colour);
+  }
 }
