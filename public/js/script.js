6f013e37724f05c3e2afd648584166dd889405a2
Nagornykh Maria <manyaknag@gmail.com>
2019-04-03 09:21:43 +0300

    делаю сохранение mark

--- public/js/script.js
+++ public/js/script.js
@@ -26,62 +26,26 @@ document.addEventListener('DOMContentLoaded', function(event) {
   let arrLi = Array.from(checkboxList.children);
 
   let checkboxes = document.querySelectorAll('[data-selector="checkbox"]');
-  // console.log('checkboxes=', checkboxes, '***', typeof checkboxes);
-
-  // let arrCheckboxes = Array.from(checkboxes);
-  // console.log('arrCheckboxes=', arrCheckboxes, '***', typeof arrCheckboxes);
   let range;
   let markNode = document.createElement("mark");
   let textDivInnerHtml = textDiv.innerHTML;
-  // console.log('textDivInnerHtml=', textDivInnerHtml);
 
   checkboxes.forEach((input) => {
     input.addEventListener('change', (event) => {
       console.log('selection.isCollapsed=', selection.isCollapsed);
+
       if (!selection || selection.isCollapsed) {
         event.target.checked = false;
         window.alert('Сначала выделите текст');
       } else {
-        // console.log('else=', selection.toString());
-        // console.log('else=', selection.anchorNode);
-        // console.log('else=', selection.anchorOffset);
-        // console.log('else=', selection.focusNode);
-        // console.log('else=', selection.focusOffset);
-        // console.log('else=', selection);
         range = selection.getRangeAt(0);
         console.log('range=', range);
         //поставить марку
-        range.surroundContents(markNode); //работает, только марк ставится 1 раз и
-        // чекбокс
-        //чекается сразу
+        //работает, только марк ставится 1 раз и чекбокс чекается сразу
+        range.surroundContents(markNode);
       }
     });
   });
-
-  /*checkboxes.forEach(function(element) {
-   console.log('element=', element);
-   element.addEventListener('change', function(event) {
-   console.log('event=', event);
-   // event.preventDefault();
-   // event.stopImmediatePropagation();
-   event.stopPropagation();
-   })
-   });*/
-
-  /*checkboxList.addEventListener('change', function(event) {
-   console.log('event.target=', event.target); //input
-   console.log('event.currentTarget=', event.currentTarget); //ul
-   //предупреждать, когда нет выделения
-   if (!selection) {
-   // window.alert('Сначала выделите текст');
-   } else {
-   // console.log('else');
-   /!*arrLi.forEach(function(element) {
-   let li = Array.from(element.children);
-   tn(li, selection);
-   });*!/
-   }
-   });*/
 });
 
 //ставим mark вкруг выделенного текста
