d3c09763ae08902bab3b0d80f8263e73fd330d9b
Nagornykh Maria <manyaknag@gmail.com>
2019-04-03 16:45:23 +0300

    сделала через hiliteColor

--- public/js/script.js
+++ public/js/script.js
@@ -28,49 +28,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
 
   buttons.forEach((btn) => {
     btn.addEventListener('click', (event) => {
-      console.log('event', event.target);
-      console.log('event', event.target.dataset);
-      console.log('event', event.target.dataset.color);
-      console.log('event', event.target.dataset.name);
       document.execCommand('hiliteColor', false, event.target.dataset.color);
-      // highlight('blue');
     });
   });
 });
-
-//для IE 9
-function makeEditableAndHighlight(colour) {
-  let range,
-    sel = window.getSelection();
-  if (sel.rangeCount && sel.getRangeAt) {
-    range = sel.getRangeAt(0);
-  }
-  document.designMode = 'on';
-  if (range) {
-    sel.removeAllRanges();
-    sel.addRange(range);
-  }
-  // Use HiliteColor since some browsers apply BackColor to the whole block
-  if (!document.execCommand('HiliteColor', false, colour)) {
-    document.execCommand('BackColor', false, colour);
-  }
-  document.designMode = 'off';
-}
-
-function highlight(colour) {
-  let range, sel;
-  if (window.getSelection) {
-    // IE9 and non-IE
-    try {
-      if (!document.execCommand('BackColor', false, colour)) {
-        makeEditableAndHighlight(colour);
-      }
-    } catch (ex) {
-      makeEditableAndHighlight(colour);
-    }
-  } else if (document.selection && document.selection.createRange) {
-    // IE <= 8 case
-    range = document.selection.createRange();
-    range.execCommand('BackColor', false, colour);
-  }
-}
