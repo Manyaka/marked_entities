00943ac4bdd32b1751c4a155e523325717864207
Nagornykh Maria <manyaknag@gmail.com>
2019-04-04 16:45:16 +0300

    получилось 👩‍

--- public/js/script.js
+++ public/js/script.js
@@ -5,12 +5,31 @@ document.addEventListener('DOMContentLoaded', function(event) {
   let resultDiv = document.querySelector('[data-selector="result-div"]');
   let resultArray = [];
   let selection;
+
+  // debounce the event
+  let selectionChangeTimer = null;
+
+  function handleSelectionChange() {
+    clearTimeout(selectionChangeTimer);
+    selectionChangeTimer = setTimeout(getSelection, 500);
+  }
+
+  function getSelection() {
+    selection = document.getSelection();
+    if (!selection.rangeCount) {
+      return;
+    }
+    console.log('getSelection:', selection.toString());
+  }
+
   document.onselectionchange = function() {
     //селекшн должен считываться только в блоке текста - containsNode()
     if (document.getSelection().containsNode(textDiv, true)) {
-      textDiv.contentEditable = true;
-      textDiv.focus();
-      selection = document.getSelection();
+      handleSelectionChange();
+      // textDiv.contentEditable = true;
+      // textDiv.focus();
+      // selection = document.getSelection();
+      // console.log(selection.toString());
     }
   };
 
@@ -25,10 +44,34 @@ document.addEventListener('DOMContentLoaded', function(event) {
   //--------------------------------------------
   //selection тоже используется в этом блоке
   let buttons = document.querySelectorAll('[data-selector="buttons"]');
+  // let range, markNode;
+  let textDivInnerHtml = textDiv.innerHTML;
+  // console.log('textDivInnerHtml--Start=', textDivInnerHtml);
 
   buttons.forEach((btn) => {
     btn.addEventListener('click', (event) => {
-      document.execCommand('hiliteColor', false, event.target.dataset.color);
+      // document.execCommand('hiliteColor', false, event.target.dataset.color);
+      let markNode = document.createElement('mark');
+      let dataName = document.createAttribute('data-name');
+      let className = document.createAttribute('class');
+      dataName.value = event.target.dataset.name;
+      className.value = event.target.dataset.class;
+      markNode.setAttributeNode(dataName);
+      markNode.setAttributeNode(className);
+
+      if (!selection || selection.isCollapsed) {
+        window.alert('Сначала выделите текст');
+      } else {
+        let range = selection.getRangeAt(0);
+        range.surroundContents(markNode);
+        textDivInnerHtml = textDiv.innerHTML;
+        // console.log('textDivInnerHtml--End=', textDivInnerHtml);
+        selection.removeAllRanges();
+
+        textDiv.innerHTML = textDivInnerHtml;
+      }
     });
   });
 });
+
+//input.setSelectionRange(1, 56);
